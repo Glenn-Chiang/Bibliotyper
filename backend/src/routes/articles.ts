5661e85905e0e41c892d745af1d483e3dc93a2c4
Glenn Chiang <glennchiang24@gmail.com>
2023-12-19 21:23:06 +0800

    Added /getArticle route

--- backend/src/routes/articles.ts
+++ backend/src/routes/articles.ts
@@ -4,6 +4,7 @@ import { instance as axios } from "../axiosConfig.js";
 const articlesRouter = Router();
 const articleLimit = 100;
 
+// Get articles by article type and category
 articlesRouter.get("/", async (req, res, next) => {
   const { articleTypeId, categoryId } = req.query;
 
@@ -14,8 +15,22 @@ articlesRouter.get("/", async (req, res, next) => {
     const articles = axiosRes.data;
     res.json(articles ? articles.articles.slice(0, articleLimit) : []);
   } catch (error) {
-    console.log(error);
+    next(error);
   }
 });
+
+// Get article by id
+articlesRouter.get("/:articleId", async (req, res, next) => {
+  const articleId = req.params.articleId
+
+  try {
+    const axiosRes = await axios.get(`/article/${articleId}/xml`)
+    const articleData = axiosRes.data
+    console.log(articleData)
+    res.json(articleData)
+  } catch (error) {
+    next(error)
+  }
+})
 
 export { articlesRouter };
