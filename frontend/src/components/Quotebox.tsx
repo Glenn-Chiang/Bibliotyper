2e880c2cb3d1799dfb531301ce4dce04060449d4
Glenn Chiang <glennchiang24@gmail.com>
2023-12-22 16:14:10 +0800

    Added underline in input box

--- frontend/src/components/Quotebox.tsx
+++ frontend/src/components/Quotebox.tsx
@@ -2,50 +2,53 @@ import { useCallback, useEffect, useState } from "react";
 
 type QuoteboxProps = {
   quote: string;
-  startGame: () => void
-  refetch: () => void
+  startGame: () => void;
+  refetch: () => void;
 };
 
 export const Quotebox = ({ quote, startGame, refetch }: QuoteboxProps) => {
-  const [inputText, setInputText] = useState("")
+  const [inputText, setInputText] = useState("");
 
   const quoteChars = quote.split("");
 
-  const handleKeydown = useCallback((event: KeyboardEvent) => {
-    // Ignore keys that are neither a character nor backspace
-    if (event.key.length !== 1 && event.key !== 'Backspace') return;
+  const handleKeydown = useCallback(
+    (event: KeyboardEvent) => {
+      // Ignore keys that are neither a character nor backspace
+      if (event.key.length !== 1 && event.key !== "Backspace") return;
 
-    // Start game when user starts typing
-    startGame()
+      // Start game when user starts typing
+      startGame();
 
-    // Backspace
-    if (event.key === "Backspace") {
-      setInputText(inputText => inputText.slice(0, inputText.length - 1))
-    // Add character
-    } else {
-      setInputText(inputText => inputText + event.key)
-    }
-  }, [startGame])
+      // Backspace
+      if (event.key === "Backspace") {
+        setInputText((inputText) => inputText.slice(0, inputText.length - 1));
+        // Add character
+      } else {
+        setInputText((inputText) => inputText + event.key);
+      }
+    },
+    [startGame]
+  );
 
   useEffect(() => {
-    document.addEventListener("keydown", handleKeydown)
+    document.addEventListener("keydown", handleKeydown);
     return () => {
-      document.removeEventListener("keydown", handleKeydown)
-    }
-  }, [handleKeydown])
+      document.removeEventListener("keydown", handleKeydown);
+    };
+  }, [handleKeydown]);
 
   // When user finishes typing current quote, refetch quote
   useEffect(() => {
     if (inputText.length === quote.length) {
-      refetch()
-      setInputText("")
+      refetch();
+      setInputText("");
     }
-  }, [inputText, quote, refetch])
+  }, [inputText, quote, refetch]);
 
   // Reset input when quote changes
   useEffect(() => {
-      setInputText("");
-  }, [quote])
+    setInputText("");
+  }, [quote]);
 
   return (
     <p className="rounded-md p-4 bg-slate-100 text-slate-600 text-lg">
@@ -62,8 +65,12 @@ type LetterProps = {
 };
 const Letter = ({ quoteChar, inputChar }: LetterProps) => {
   const grade = inputChar === quoteChar ? 2 : inputChar ? 1 : 0;
-  const gradeColors = ["text-slate-500", "text-rose-500", "text-teal-500"];
-  const color = gradeColors[grade];
+  const gradeStyles = [
+    "text-slate-500",
+    "text-rose-500 border-b-2 border-rose-500",
+    "text-teal-500 border-b-2 border-teal-500",
+  ];
+  const style = gradeStyles[grade];
 
-  return <span className={color}>{quoteChar}</span>;
+  return <span className={style}>{quoteChar}</span>;
 };
