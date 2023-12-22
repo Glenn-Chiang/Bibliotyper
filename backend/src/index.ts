936f145f45e88e87de58f973760fd56ccb257be0
Glenn Chiang <glennchiang24@gmail.com>
2023-12-22 21:01:48 +0800

    Added endpoints to get scores

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,15 @@ import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
 import { configDotenv } from 'dotenv'
+import { scoresRouter } from './routes/scores.js'
 configDotenv()
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(morgan("dev"))
+
+app.use(scoresRouter)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
--- backend/src/routes/scores.ts
+++ backend/src/routes/scores.ts
@@ -1,7 +1,81 @@
 import { Router } from "express";
+import { prisma } from "../db.js";
 
 const scoresRouter = Router();
 
-scoresRouter.get("/", async (req, res, next) => {
-  const userId = 1;
+// Get user's scores under the selected timeLimit, ordered from latest to oldest
+scoresRouter.get("/users/:userId/scores", async (req, res, next) => {
+  const userId = Number(req.params.userId);
+  const time = req.query.time ? Number(req.query.time) : undefined;
+  const sortParam = req.query.sort;
+  const sort =
+    sortParam === "newest" || sortParam === "highest" ? sortParam : undefined;
+
+  try {
+    const scores = await prisma.score.findMany({
+      where: {
+        userId,
+        time,
+      },
+      orderBy:
+        sort === "highest"
+          ? {
+              wpm: "desc",
+            }
+          : {
+              dateAdded: "desc",
+            },
+    });
+    res.json(scores);
+  } catch (error) {
+    next(error);
+  }
 });
+
+// Get user's high score under the selected timeLimit
+scoresRouter.get("/users/:userId/highScore", async (req, res, next) => {
+  const userId = Number(req.params.userId);
+  const time = req.query.time ? Number(req.query.time) : undefined;
+
+  try {
+    const highScore = (
+      await prisma.score.aggregate({
+        _max: {
+          wpm: true,
+        },
+        where: {
+          userId,
+          time,
+        },
+      })
+    )._max;
+    res.json(highScore);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Get top x highest scores across all users under the selected timeLimit
+scoresRouter.get("/scores", async (req, res, next) => {
+  const time = req.query.time ? Number(req.query.time) : undefined;
+
+  try {
+    const scores = await prisma.score.findMany({
+      where: {
+        time,
+      },
+      include: {
+        user: true,
+      },
+      orderBy: {
+        wpm: "desc",
+      },
+    });
+  
+    res.json(scores)
+  } catch (error) {
+    next(error)
+  }
+});
+
+export { scoresRouter };
