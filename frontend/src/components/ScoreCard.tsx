c2cbe6fbca46f4f6b666fc59f0283f9288d85fac
Glenn Chiang <glennchiang24@gmail.com>
2023-12-22 17:40:00 +0800

    Built ScoreCard

--- frontend/src/components/ScoreCard.tsx
+++ frontend/src/components/ScoreCard.tsx
@@ -1,23 +1,46 @@
-type ScoreCardProps = {
-  totalKeystrokes: number,
-  correctKeystrokes: number,
-  time: number
-}
+import { faTrophy } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export const ScoreCard = ({totalKeystrokes, correctKeystrokes, time}: ScoreCardProps) => {
-  const cpm = correctKeystrokes / (time / 60)
-  const wpm = Math.round(cpm / 5)
-  const accuracy = Math.round(correctKeystrokes / totalKeystrokes * 100)
+type ScoreCardProps = {
+  totalKeystrokes: number;
+  correctKeystrokes: number;
+  time: number;
+};
+
+export const ScoreCard = ({
+  totalKeystrokes,
+  correctKeystrokes,
+  time,
+}: ScoreCardProps) => {
+  const cpm = correctKeystrokes / (time / 60);
+  const wpm = Math.round(cpm / 5);
+  const accuracy = Math.round((correctKeystrokes / totalKeystrokes) * 100);
+
+  // todo: save score and get personal best
 
   return (
-    <section className="rounded-md p-4 border-2">
-      Your score
-      <div>
-        <span>{wpm}</span> WPM
-      </div>
-      <div>
-        <span>{accuracy}%</span> accuracy
-      </div>
-    </section>
-  )
-}
+    <div className="flex flex-col sm:flex-row gap-2">
+      <section className="sm:w-1/2 flex justify-center items-center flex-col gap-2 rounded-md p-4 border-2 text-slate-500">
+        <span>Your score</span>
+        <div className="text-2xl text-sky-500">
+          <span>{wpm}</span> WPM
+        </div>
+        <div>
+          <span>{accuracy}%</span> accuracy
+        </div>
+      </section>
+      <section className="sm:w-1/2 flex justify-center items-center flex-col gap-2 rounded-md p-4 border-2 text-slate-500">
+        <span className="flex gap-2 items-center">
+          <FontAwesomeIcon icon={faTrophy} className="text-amber-400"/>
+          Personal best
+        </span>
+        <div className="text-2xl text-sky-500">
+          <span>{wpm}</span> WPM
+        </div>
+        <div>
+          <span>{accuracy}%</span> accuracy
+        </div>
+      </section>
+    </div>
+  );
+};
